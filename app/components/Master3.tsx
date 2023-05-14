5b9287cdc8b9b78fce38baaeafc5ac3254c6e060
xychenxy <mavischen916@gmail.com>
2023-05-14 19:44:01 +1000

    resolve conflict 3

--- app/components/Master3.tsx
+++ app/components/Master3.tsx
@@ -4,7 +4,7 @@ export default function Header() {
 		<div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
 			<div className="text-center mt-10">
 				<h1 className="text-white text-5xl font-bold mb-2">
-					master 3 main 3
+					master 3 main 3 master 3{" "}
 				</h1>
 				<SearchBar />
 			</div>
