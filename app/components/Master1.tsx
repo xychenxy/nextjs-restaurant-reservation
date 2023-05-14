dde097f5d4401ff940916347da2c9562564c94de
xychenxy <mavischen916@gmail.com>
2023-05-14 19:45:09 +1000

    main 1

--- app/components/Master1.tsx
+++ app/components/Master1.tsx
@@ -3,7 +3,9 @@ export default function Header() {
 	return (
 		<div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
 			<div className="text-center mt-10">
-				<h1 className="text-white text-5xl font-bold mb-2">Master 1</h1>
+				<h1 className="text-white text-5xl font-bold mb-2">
+					Master 1 r
+				</h1>
 				<SearchBar />
 			</div>
 		</div>
